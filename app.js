37d2a7c79296f789324076e6dc2f51aa5c959af4
Mohana Vamsi <mohanavamsi14@gmail.com>
2024-02-13 15:38:59 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
-const strikeButton = document.getElementById("strike");
-const resetButton = document.getElementById("reset");
-const $team1Score = document.getElementById("st1");
-const $team1Wickets = document.getElementById("wt1");
-const $team2Score = document.getElementById("st2");
-const $team2Wickets = document.getElementById("wt2");
-
-const strikeAudio = new Audio("http://bit.ly/so-ball-hit");
-const gameOverAudio = new Audio("http://bit.ly/so-crowd-cheer");
-
+const strikeButton = document.getElementById("stri");
+const resetButton = document.getElementById("replay");
+const Team1Score = document.getElementById("st1");
+const Team1Wickets = document.getElementById("wt1");
+const Team2Score = document.getElementById("st2");
+const Team2Wickets = document.getElementById("wt2");
 var team1Score = 0;
 var team1Wickets = 0;
 var team2Score = 0;
@@ -22,14 +18,14 @@ function gameOver() {
   gameOverAudio.play();
   if (team1Score > team2Score) alert("IND wins");
   if (team2Score > team1Score) alert("PAK wins");
-  if (team2Score === team1Score) alert("It is another superover!");
+  if (team2Score === team1Score) alert("It is draw!");
 }
 
 function updateScore() {
-  $team1Score.textContent = team1Score;
-  $team1Wickets.textContent = team1Wickets;
-  $team2Score.textContent = team2Score;
-  $team2Wickets.textContent = team2Wickets;
+  Team1Score.textContent = team1Score;
+  Team1Wickets.textContent = team1Wickets;
+  Team2Score.textContent = team2Score;
+  Team2Wickets.textContent = team2Wickets;
 }
 
 resetButton.onclick = () => {
@@ -37,32 +33,24 @@ resetButton.onclick = () => {
 };
 
 strikeButton.onclick = () => {
-  //play audio
   strikeAudio.pause();
   strikeAudio.currentTime = 0;
   strikeAudio.play();
 
-  //generate random strike value
   const randomElement =
     possibleOutcomes[Math.floor(Math.random() * possibleOutcomes.length)];
 
-  //second batting
-  if (turn === 2) {
-    //increase ball count
+  if (turn == 2) {
     team2BallsFaced++;
-    //update score for the ball
     document.querySelector(
-      `#team2-superover div:nth-child(${team2BallsFaced})`
+      `#t2balls div:nth-child(${team2BallsFaced})`
     ).textContent = randomElement;
-    // if wicket, update wickets variable
     if (randomElement === "W") {
       team2Wickets++;
     }
-    // else update score
     else {
       team2Score += randomElement;
     }
-    // Game over condition
     if (
       team2BallsFaced === 6 ||
       team2Wickets === 2 ||
@@ -76,7 +64,7 @@ strikeButton.onclick = () => {
   if (turn === 1) {
     team1BallsFaced++;
     document.querySelector(
-      `#team1-superover div:nth-child(${team1BallsFaced})`
+      `#t1balls div:nth-child(${team1BallsFaced})`
     ).textContent = randomElement;
     if (randomElement === "W") {
       team1Wickets++;
